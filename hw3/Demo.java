f4f553fcce0445912dc33fc7ed81f97c92417294
Ilya <fillomaniya@mail.ru>
2023-04-27 13:45:31 +0300

    add hw3

---
+++ hw3/Demo.java
@@ -0,0 +1,10 @@
+package hw3;
+
+import hw3.gui.Login;
+
+public class Demo {
+    public static void main(String[] args) {
+        new Login();
+
+    }
+}
---
+++ hw3/exceptions/WrongPasswordException.java
@@ -0,0 +1,19 @@
+package hw3.exceptions;
+
+public class WrongPasswordException extends Exception {
+    private String detail;
+
+    public WrongPasswordException() {
+        detail = "Incorrect password";
+    }
+
+    public WrongPasswordException(String message) {
+        super(message);
+        detail = message;
+    }
+
+    @Override
+    public String toString() {
+        return "WrongPasswordException: " + detail;
+    }
+}
